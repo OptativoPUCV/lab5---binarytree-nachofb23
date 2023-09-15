1c89b9ce18d0f52bebe7d9fb261b26c5a555e3e4
Replit user <>
2023-09-15 00:09:22 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -155,5 +155,26 @@ Pair * firstTreeMap(TreeMap * tree) {
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
+  if (tree == NULL || tree->current == NULL) {
     return NULL;
+  }
+
+  TreeNode* current = tree->current;
+
+  if (current->right != NULL) {
+    current = current->right;
+    while (current->left != NULL) {
+      current = current->left;
+    }
+    tree->left = current;
+    return current->pair;
+  } else {
+    TreeNode* parent = current->parent;
+    while (parent == NULL && current == parent->right) {
+      current = parent;
+      parent = current->right;
+    }
+    tree->current = parent;
+    return (parent != NULL) ? parent->pair : NULL;
+  }
 }
